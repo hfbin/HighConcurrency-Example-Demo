f688b8fc1f5575a294183e99a9391ca438b758f1
hfbin <1716850375@qq.com>
2018-05-29 09:35:15 +0800

    ConcurrencyTest

--- src/main/java/cn/hfbin/concurrency/ConcurrencyApplication.java
+++ src/main/java/cn/hfbin/concurrency/ConcurrencyApplication.java
@@ -1,12 +1,26 @@
 package cn.hfbin.concurrency;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
+@Slf4j
 public class ConcurrencyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcurrencyApplication.class, args);
 	}
+	@ResponseBody
+	@RequestMapping("/test")
+	public String test(){
+		log.info("test");
+		return "rets";
+	}
+
+
 }
