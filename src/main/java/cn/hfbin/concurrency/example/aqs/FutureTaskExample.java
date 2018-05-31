68a09c52038d5f40c105f5a39de3f94abd92e50d
hfbin <1716850375@qq.com>
2018-05-31 19:05:01 +0800

    AQS module

---
+++ src/main/java/cn/hfbin/concurrency/example/aqs/FutureTaskExample.java
@@ -0,0 +1,31 @@
+package cn.hfbin.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by: HuangFuBin
+ * Such description: FutureTask 可以得到线程的返回值 ，FutureTask 实现了 Runnable, Future<V> 接口，所以拥有Runnable, Future<V>的功能
+ */
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result：{}", result);
+    }
+}
